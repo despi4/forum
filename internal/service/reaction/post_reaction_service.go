c8a87cb946d4690448bbb8c14eff83308c4057f9
despi4 <johapo62@gmail.com>
2026-06-17 17:44:10 +0500

    minor changes

--- internal/service/reaction/post_reaction_service.go
+++ internal/service/reaction/post_reaction_service.go
@@ -1,1 +1,11 @@
 package reactionsvc
+
+import domain "01.tomorrow-school.ai/git/amadiuly/forum/internal/domain/reaction"
+
+type PostReactionService struct {
+	postReactionRepository domain.PostReactionRepository
+}
+
+func NewPostReactionService() *PostReactionService {
+	return
+}
